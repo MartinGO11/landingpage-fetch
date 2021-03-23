0be6aaba43c0c0126d6cc8c97d74361d4f5ca92f
MartinGO11 <ogutierrez.m94@gmail.com>
2021-03-23 15:01:23 -0300

    rdy

--- src/App.js
+++ src/App.js
@@ -1,22 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import "./App.css";
+import FormularioTareas from "./components/FormularioTarea";
+import ListaTarea from "./components/ListaTarea";
 
 function App() {
+  const [tareas, setTarea] = useState ([{ label: "", done: false }]);
+
+/*function  agregarTarea (tarea) {
+    setTarea([tarea,...tareas]);
+  }*/
+ 
+  function eliminarTarea(id){
+    setTarea(tareas.filter(tarea => tarea.id !== id));
+  }
+
+const agregarTarea = (tarea) => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/martingo1", {
+			method: "GET",
+			headers: {
+				"content-type": "application/json"
+			}
+		})
+			.then(res => res.json())
+			.then(response => {
+				setTarea(
+					response.map((item, index) => {
+						return item;
+					})
+				);
+			});
+    };
+    const crearTarea = () => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/martingo2", {
+			method: "POST",
+			headers: {
+				"content-type": "application/json"
+			},
+			body: []
+		})
+			.then(response => {
+				console.log(response);
+			})
+			.catch(err => {
+				console.log(err);
+			});
+    };
+    
+    const updateTarea = () => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/martingo3", {
+			method: "PUT",
+			headers: {
+				"content-type": "application/json"
+			},
+			body: JSON.stringify(ListaTarea)
+		})
+			.then(response => {
+				console.log(response);
+			})
+			.catch(err => {
+				console.log(err);
+			});
+    };    
+
+    useEffect(() => {
+        agregarTarea();
+        updateTarea ();    
+        crearTarea ();
+	}, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Task</h1>
+        <FormularioTareas agregarTarea={agregarTarea} />
+        <ListaTarea tareas={tareas} eliminarTarea={eliminarTarea}/>
+              {(() => {
+        if (tareas >= 0) {
+          return (
+            <div>No tasks, add a task</div>
+          )
+        } else {
+          return (
+            <div>{tareas.length} Pendent tasks</div>
+          )
+        }}
+      )()}
       </header>
     </div>
   );
